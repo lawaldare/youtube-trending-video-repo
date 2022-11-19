1e95d9d122f044467af6b0c3a2a3f55ad690a7df
Dare Lawal <dare.lawal@originmarkets.com>
2022-11-19 22:29:35 +0000

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,52 @@
-import { YoutubeService } from './youtube.service';
-import { Component, OnInit } from '@angular/core';
-import { DomSanitizer} from '@angular/platform-browser';
-
-
+import { YoutubeService } from "./youtube.service";
+import { Component, OnInit } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
+import { map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  title = 'youtube-trending';
-  countries: any;
+  title = "youtube-trending";
+  countries$ = this.youtubeService.getAllCountry().pipe(
+    map((countries) => {
+      return countries.map((country) => ({
+        name: country.name.common,
+        code: country.cca2,
+      }));
+    })
+  );
   countryTrendingYoutubeVideos: any[];
   p = 1;
 
-
-
-
-  constructor(private youtubeService: YoutubeService, private sanitizer: DomSanitizer){}
-
+  constructor(
+    private youtubeService: YoutubeService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit() {
-
-    this.getCountries();
     this.defaultNigeria();
   }
 
-
   selectCountry(event) {
     let alpha2code = event.target.value;
-    this.youtubeService.getYoutubeTrendingVideos(alpha2code).subscribe(data => {
-      this.countryTrendingYoutubeVideos = data.items;
-      console.log(this.countryTrendingYoutubeVideos);
-    });
+    this.youtubeService
+      .getYoutubeTrendingVideos(alpha2code)
+      .subscribe((data) => {
+        this.countryTrendingYoutubeVideos = data.items;
+        console.log(this.countryTrendingYoutubeVideos);
+      });
   }
 
   defaultNigeria() {
-    this.youtubeService.getNigeriaYoutubeTrendingVideos().subscribe(data => {
+    this.youtubeService.getNigeriaYoutubeTrendingVideos().subscribe((data) => {
       this.countryTrendingYoutubeVideos = data.items;
-    });
-  }
-
-  getCountries() {
-    this.youtubeService.getAllCountry().subscribe(data => {
-      this.countries = data;
     });
   }
 
   getVideo(id: string) {
     return `https://www.youtube.com/embed/${id}`;
   }
-
-
 }
--- src/app/youtube.service.ts
+++ src/app/youtube.service.ts
@@ -1,49 +1,44 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
-
-
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class YoutubeService {
+  apiKey = "AIzaSyDjltnL2Wyi7UdjkySyhyYsNlG0zBP6SqY";
+  maxResults = "50";
+  apiBaseURL = "https://www.googleapis.com/youtube/v3/videos";
 
-  apiKey = 'AIzaSyDAg_JYmtAbIcNRgywofmxczJPMmpMuTLs';
-  maxResults = '50';
-  apiBaseURL = 'https://www.googleapis.com/youtube/v3/videos';
-
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient) {}
 
   getYoutubeTrendingVideos(alpha2code: string): Observable<any> {
     return this.http.get(this.apiBaseURL, {
       params: new HttpParams()
-        .append('part', 'snippet')
-        .append('chart', 'mostPopular')
-        .append('kind', 'youtube#videoListResponse')
-        .append('maxResults', this.maxResults)
-        .append('regionCode', alpha2code)
-        .append('key', this.apiKey)
+        .append("part", "snippet")
+        .append("chart", "mostPopular")
+        .append("kind", "youtube#videoListResponse")
+        .append("maxResults", this.maxResults)
+        .append("regionCode", alpha2code)
+        .append("key", this.apiKey),
     }) as Observable<any>;
   }
 
   getNigeriaYoutubeTrendingVideos(): Observable<any> {
     return this.http.get(this.apiBaseURL, {
       params: new HttpParams()
-        .append('part', 'snippet')
-        .append('chart', 'mostPopular')
-        .append('kind', 'youtube#videoListResponse')
-        .append('maxResults', this.maxResults)
-        .append('regionCode', 'NG')
-        .append('key', this.apiKey)
+        .append("part", "snippet")
+        .append("chart", "mostPopular")
+        .append("kind", "youtube#videoListResponse")
+        .append("maxResults", this.maxResults)
+        .append("regionCode", "GB")
+        .append("key", this.apiKey),
     }) as Observable<any>;
   }
 
-
-
   getAllCountry(): Observable<any> {
-    return this.http.get('https://restcountries.eu/rest/v2/all') as Observable<any>;
+    return this.http.get(
+      "https://restcountries.com/v3.1/all"
+    ) as Observable<any>;
   }
 }
-
